c6ca5fe9d1cd9f4596b04d815dfcaf202664f149
Suhail0206327 <122574657+suhail3728@users.noreply.github.com>
2024-10-18 18:08:50 -0700

    Create shift page header

--- src/screens/ShiftsScreen.js
+++ src/screens/ShiftsScreen.js
@@ -1,27 +1,56 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button, Text , StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import Colors from '../constants/colors';
-
+import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const ShiftsScreen = () => {
-    return(
-
-        <View>
-            <Text style={styles.text}>Hey Shifts</Text>
+  const currentMonth = new Date().toLocaleDateString('default', {
+    month: 'long',
+  });
+  return (
+    <View style={{flex: 1, backgroundColor: Colors.pureWhite, padding: 30}}>
+      <View style={{display:'flex', flexDirection:'row', justifyContent:'space-between'}} >
+        <TouchableOpacity>
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+            }}>
+            <Text style={{color: Colors.darkGray, fontSize: 18}}>
+              {' '}
+              {currentMonth}
+            </Text>
+            <MaterialIcon
+              name="chevron-down"
+              size={20}
+              color={Colors.darkGray}></MaterialIcon>
+          </View>
+        </TouchableOpacity>
+       
+        <View style={{display:'flex', flexDirection:'row' , gap:20,}}>
+            <TouchableOpacity
+       
+        >
+            <MaterialIcon name='calendar-month-outline' color={Colors.darkGray} size={20}></MaterialIcon>
+        </TouchableOpacity>
+    
+        <TouchableOpacity
+       
+       >
+           <MaterialIcon name='plus' color={Colors.darkGray} size={20}></MaterialIcon>
+       </TouchableOpacity>
+        
         </View>
-    );
-}
+      </View>
+    </View>
+  );
+};
 
 export default ShiftsScreen;
 
-const styles =StyleSheet.create({
-
-    text: {
-
-        color: Colors.green,
-        
-    }
+const styles = StyleSheet.create({
+  text: {
+    color: Colors.green,
+  },
 });
-
-
-
