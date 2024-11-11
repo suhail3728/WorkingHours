7e86190b5f0a5cc0e62ac92c300041ac40b93004
Suhail0206327 <122574657+suhail3728@users.noreply.github.com>
2024-11-11 00:54:47 -0800

    Change the icon to outline and align the input container to center

--- src/screens/signIn.js
+++ src/screens/signIn.js
@@ -2,18 +2,16 @@ import React, {useState, useContext} from 'react';
 import {View, TextInput, Button, Text, StyleSheet} from 'react-native';
 import {signInWithEmailAndPassword} from 'firebase/auth';
 import {auth} from '../config/firebase';
-import { AuthContext } from '../navigation/AuthContext';
-import { CustomButton } from '../components/CustomButton';
+import {AuthContext} from '../navigation/AuthContext';
+import {CustomButton} from '../components/CustomButton';
 import Colors from '../constants/colors';
-import Icon from 'react-native-vector-icons/MaterialIcons'; 
-
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const SignInScreen = ({navigation}) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const { setUserId } = useContext(AuthContext);
-
+  const {setUserId} = useContext(AuthContext);
 
   const handleSignIn = async () => {
     const userCredential = await signInWithEmailAndPassword(
@@ -23,12 +21,8 @@ const SignInScreen = ({navigation}) => {
     );
     const user = userCredential.user;
 
-    console.log(`User signed in:${(user.uid)}`);
+    console.log(`User signed in:${user.uid}`);
     setUserId(user.uid);
-
- 
-    
-    
   };
 
   return (
@@ -42,30 +36,29 @@ const SignInScreen = ({navigation}) => {
         onChangeText={setEmail}
         keyboardType="email-address"
       />
-     <View style={styles.inputContainer}>
-  <Icon 
-    name="lock" 
-    size={20} 
-    color="#666"
-    style={styles.icon}
-  />
-  <TextInput
-    placeholder="Enter your password"
-    placeholderTextColor= {Colors.darkGray}
-    value={password}
-    style={styles.textInput}
-    onChangeText={setPassword}
-    secureTextEntry
-  />
-</View>
+      <View style={styles.inputContainer}>
+        <Icon
+          name="lock-outline"
+          size={20}
+          color={Colors.black}
+          style={styles.icon}
+        />
+        <TextInput
+          placeholder="Password"
+          placeholderTextColor={Colors.black}
+          value={password}
+          style={styles.textInput}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+      </View>
       {error ? <Text>{error}</Text> : null}
       <CustomButton
         title="Log In"
         backgroundColor={Colors.green}
         borderColor={Colors.welcomeScreenBg}
         textColor={Colors.welcomeScreenBg}
-        onPress={()=> handleSignIn}></CustomButton>
-     
+        onPress={() => handleSignIn}></CustomButton>
     </View>
   );
 };
@@ -73,34 +66,30 @@ const SignInScreen = ({navigation}) => {
 export default SignInScreen;
 
 const styles = StyleSheet.create({
-
   container: {
-   backgroundColor: Colors.welcomeScreenBg,
-   alignItems:'center',
+    backgroundColor: Colors.welcomeScreenBg,
+    alignItems: 'center',
   },
   heading: {
-    color:Colors.black,
-    fontWeight:'900',
-    fontSize:37,
-    marginBottom:70,
-
+    color: Colors.black,
+    fontWeight: '900',
+    fontSize: 37,
+    marginBottom: 70,
   },
   inputContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    borderBottomWidth: 1,  
+    justifyContent: 'flex-start',
+    borderBottomWidth: 1,
     borderColor: Colors.black,
-    backgroundColor: Colors.green,
-   width:248,
-   height:50,
+    backgroundColor: Colors.welcomeScreenBg,
+    width: 248,
+    height: 50,
+    gap: 13,
   },
 
   textInput: {
-    paddingVertical: 15,
-    fontSize: 16,
-    paddingLeft: 0,
-
-
+    fontSize: 17,
   },
   text: {
     margin: 10,
